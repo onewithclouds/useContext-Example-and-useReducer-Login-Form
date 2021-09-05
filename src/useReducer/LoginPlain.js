c34aa1f4b21cb5eb5346aa2cb67afd8c4e430df2
onewithclouds <onewithclouds@protonmail.ch>
2021-09-05 19:53:50 +0300

    state introduced email/password

--- src/useReducer/LoginPlain.js
+++ src/useReducer/LoginPlain.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react'
 
+
+
 export default function LoginPlain() {
+
+    const [ username, setUsername ] = useState('')
+    const [ password, setPassword ] = useState('')
 
     const onSubmit = async e => {
         e.preventDefault();
@@ -16,11 +21,16 @@ export default function LoginPlain() {
             <div className="login-container">
                 <form className="form" onSubmit={onSubmit}>
                     <p>Please Login</p>
-                    <input type="text" placeholder="username" />
+                    <input type="text" placeholder="username" 
+                        value={username} 
+                        onChange={ e => setUsername(e.currentTarget.value)} 
+                    />
                     <input
                         type="password"
                         placeholder="password"
                         autoComplete="new-password"
+                        value={password}
+                        onChange={ e => setPassword(e.currentTarget.value) }
                     />
                     <button className="submit" type="submit">
                         Log In
