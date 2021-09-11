697a3350230bd9915c1ad37743c4f5a0fe6ba5cd
onewithclouds <onewithclouds@protonmail.ch>
2021-09-11 18:13:32 +0300

    Migrating 5 useStates into 1 useReducer Completed

--- src/useReducer/LoginPlain.js
+++ src/useReducer/LoginPlain.js
@@ -1,36 +1,103 @@
-import React, { useState } from 'react'
+import React, { useReducer } from 'react'
 import { login } from './utils'
+
+function loginReducer(state, action) {
+    switch (action.type) {
+        case 'form-onSubmit': {
+            return {
+                ...state,
+                [action.field]: action.value,
+            }
+        }
+        case 'login': {
+            return {
+                ...state,
+                isLoading: true ,
+                error: '',
+            }
+        }
+        case 'login-success': {
+            return {
+                ...state,
+                isLoggedIn: true ,
+            }
+        }
+        case 'logout': {
+            return {
+                ...state,
+                isLoggedIn: false ,
+//                username: '',
+                password: '',
+                isLoading: false,
+                error: '',
+            }
+        }
+        case 'error-message': {
+            return {
+                ...state,
+                [action.field]: action.value,
+                username: '',
+                password: '',
+                isLoading: false,
+            }
+        }
+        
+        default:
+            break;
+    }
+
+    return state;
+}
+
+const initialState = {
+    username   : '' ,
+    password   : '' ,
+    isLoading  : false ,
+    error      : '' ,
+    isLoggedIn : false ,
+}
 
 
 
 export default function LoginPlain() {
 
-    const [ username, setUsername ] = useState('')
-    const [ password, setPassword ] = useState('')
-    const [ isLoading, setIsLoading ] = useState(false)
-    const [ error, setError ] = useState('')
-    const [ isLoggedIn, setIsLoggedIn ] = useState(false)
+    const [state, dispatch] = useReducer(loginReducer, initialState) 
+
+    const {username, password, isLoading, error, isLoggedIn } = state
+    
+    // const [ username, setUsername ] = useState('')
+    // const [ password, setPassword ] = useState('')
+    // const [ isLoading, setIsLoading ] = useState(false)
+    // const [ error, setError ] = useState('')
+    // const [ isLoggedIn, setIsLoggedIn ] = useState(false)
 
     const onSubmit = async e => {
         e.preventDefault()
 
-        setIsLoading(true)
+        dispatch({ type: 'login' })
 
         try {
             await login( {username, password} )
-            setIsLoggedIn(true)
-//            setUsername('')
-            setPassword('')
-            setError('')
+
+//          setIsLoading(true)
+
+            dispatch({ type: 'login-success' })
+ //           setPassword('')
+ //           setError('')
         }
         catch (error) {
 
-            setError(error)
+            dispatch({ 
+                type: 'error-message', 
+                field: 'error', 
+                value: error ,
+            })
+//            setError(error)
             console.log(error)
-
+            
         }
 
-        setIsLoading(false)
+//        setIsLoading(false)
 
     }
 
@@ -42,23 +109,36 @@ export default function LoginPlain() {
                 {isLoggedIn ? (
                     <>
                         <h1>Hello {username}</h1>
-                        <button onClick={ ()=> setIsLoggedIn(false) }>Log Out</button>
+                        <button onClick={ () => dispatch({ type: 'logout' }) }>Log Out</button>
                     </>
                 ) : 
                 (
                 <form className="form" onSubmit={onSubmit}>
                     {error && <p className="error">{error}</p>}
                     <p>Please Login</p>
-                    <input type="text" placeholder="username" 
+                    <input type="text" 
+                        placeholder="username" 
                         value={username} 
-                        onChange={ e => setUsername(e.currentTarget.value)} 
+                        onChange={ e => 
+                            dispatch({ 
+                                type: 'form-onSubmit' ,
+                                field: 'username' ,
+                                value: e.currentTarget.value ,
+                            })
+                        }
                     />
                     <input
                         type="password"
                         placeholder="password"
                         autoComplete="new-password"
                         value={password}
-                        onChange={ e => setPassword(e.currentTarget.value) }
+                        onChange={ e => 
+                            dispatch({ 
+                                type: 'form-onSubmit' ,
+                                field: 'password' ,
+                                value: e.currentTarget.value ,
+                            })
+                        }
                     />
                     <button className="submit" type="submit" disabled={isLoading}>
                         {isLoading ? 'Loggin in...' : 'Log in'}
